ba4ca510d21f8b99e9127477e73f060b5069dd51
Dong-Wan Kim <docong0120@gmail.com>
2024-09-28 23:05:10 +0900

    feat: login screen updated
    로그인 화면 색상 추가
    
    Signed-off-by: Dong-Wan Kim <docong0120@gmail.com>

--- lib/0_screens/1_login/login_screen.dart
+++ lib/0_screens/1_login/login_screen.dart
@@ -10,13 +10,16 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
   bool _isLoading = false; // 로딩 상태
 
+  /*
+   * 로그인 함수
+  */
   Future<void> _login() async {
     setState(() {
       _isLoading = true;
     });
 
     // 여기에 실제 로그인 로직 추가 (Firebase 등)
-    await Future.delayed(Duration(seconds: 2)); // 가짜 로그인 로직 (2초 지연)
+    await Future.delayed(const Duration(seconds: 2)); // 가짜 로그인 로직 (2초 지연)
 
     setState(() {
       _isLoading = false;
@@ -27,12 +30,12 @@ class _LoginPageState extends State<LoginPage> {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text("Login Success"),
-          content: Text("You have successfully logged in."),
+          title: const Text("Login Success"),
+          content: const Text("You have successfully logged in."),
           actions: [
             TextButton(
               onPressed: () => Navigator.of(context).pop(),
-              child: Text("OK"),
+              child: const Text("OK"),
             ),
           ],
         );
@@ -46,45 +49,63 @@ class _LoginPageState extends State<LoginPage> {
       resizeToAvoidBottomInset: true,
       backgroundColor: const Color.fromARGB(255, 20, 37, 26),
       appBar: AppBar(
-        title: Text("Login"),
+        backgroundColor: const Color.fromARGB(255, 20, 37, 26),
+        title: const Text(
+          "로그인",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Spacer(flex: 2),
+            const Spacer(flex: 2),
             Image.asset("assets/images/logo/logo_white_moon.png"),
-            Spacer(flex: 1),
+            const Spacer(flex: 1),
             TextField(
               controller: _idController,
-              decoration: InputDecoration(
-                labelText: "ID",
+              decoration: const InputDecoration(
+                labelText: "계정",
                 border: OutlineInputBorder(),
                 fillColor: Colors.white,
                 filled: true,
               ),
               keyboardType: TextInputType.emailAddress,
             ),
-            SizedBox(height: 28),
+            const SizedBox(height: 28),
             TextField(
               controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: "Password",
+              decoration: const InputDecoration(
+                labelText: "비밀번호",
                 border: OutlineInputBorder(),
                 fillColor: Colors.white,
                 filled: true,
               ),
               obscureText: true,
             ),
-            SizedBox(height: 28),
+            const SizedBox(height: 28),
             _isLoading
-                ? CircularProgressIndicator() // 로딩 중일 때 표시
-                : ElevatedButton(
-                    onPressed: _login,
-                    child: Text("Login"),
+                ? const CircularProgressIndicator() // 로딩 중일 때 표시
+                : SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                          ),
+                          backgroundColor: Color.fromARGB(255, 222, 211, 27)),
+                      onPressed: _login,
+                      child: const Text(
+                        "로그인",
+                        style: TextStyle(
+                            color: const Color.fromARGB(255, 20, 37, 26)),
+                      ),
+                    ),
                   ),
-            Spacer(flex: 2),
+            const Spacer(flex: 2),
           ],
         ),
       ),
